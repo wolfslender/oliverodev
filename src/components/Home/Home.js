f1d8b882999d2afe5364372cc821820e3a3cf86d
Alexis Olivero <olivero_canario@live.com>
2024-02-21 13:19:10 -0400

    Cambios en el home
    se ajustaron los elementos del home, titulo y texto y background

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,28 +9,13 @@ const Home = () => {
     from: { opacity: 0 },
   });
 
-  const titleSpring = useSpring({
-    opacity: 1,
-    from: { opacity: 0 },
-  });
-
-  const textSpring = useSpring({
-    opacity: 1,
-    marginTop: 0,
-    from: { opacity: 0, marginTop: -20 },
-  });
-
   return (
     <animated.div className="home" style={containerSpring}>
-      <div className="background-container">
-        {/* Puedes agregar estilos específicos para el fondo aquí */}
-      </div>
+      <div className="background-container"></div>
       <div className="overlay"></div>
       <div className="content">
-        <animated.h2 style={titleSpring}>Bienvenido a mi sitio web</animated.h2>
-        <animated.p style={textSpring}>
-          Soy un desarrollador web con más de 6 años de experiencia, especializado en crear soluciones web innovadoras y atractivas.
-        </animated.p>
+        <h1>Bienvenidos a Mi Portfolio</h1>
+        <p>Soy un apasionado desarrollador web con más de 6 años de experiencia, especializado en crear soluciones web innovadoras y atractivas.</p>
       </div>
     </animated.div>
   );
